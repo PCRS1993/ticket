9551d73e5f369141eacd449b557c185bea274975
pcrs <cheikhrene93@gmail.com>
2019-10-15 21:11:41 +0000

    listeapprenants

---
+++ Views/newapprenant.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+     <div class="container-fluid px-0">
+     <!-- Barre de Navigation -->
+      
+       
+       
+     <nav class="navbar navbar-expand-lg navbar-success bg-primary">
+       
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+       </button>
+       <div class="collapse navbar-collapse" id="navbarNavDropdown">
+         <ul class="navbar-nav">
+           
+             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Utilisateurs
+             </a>
+             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+               <a class="dropdown-item" href="#">Promo 3</a>
+               <a class="dropdown-item" href="#">Promo 4</a>
+               <a class="dropdown-item" href="#">Promo 5</a>
+               <a class="dropdown-item" href="#">Promo 6</a>
+               <a class="dropdown-item" href="#">Promo 7</a>
+               <a class="dropdown-item" href="#">Promo 8</a>
+
+         </div>
+             </ul>
+             
+       </div>
+</nav>      
+       
+       
+       
+       
+       
+       <nav class="navbar navbar-expand-lg navbar-success bg-primary">
+       
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Sessions
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Promo 3</a>
+                <a class="dropdown-item" href="#">Promo 4</a>
+                <a class="dropdown-item" href="#">Promo 5</a>
+                <a class="dropdown-item" href="#">Promo 6</a>
+                <a class="dropdown-item" href="#">Promo 7</a>
+                <a class="dropdown-item" href="#">Promo 8</a>
+
+          </div>
+              </ul>
+              
+        </div>
+</nav>
+      
+         <!-- Logo-->
+       <div class="row">
+           <img src="../Public/img/logo.jpg" class="img-fluid mw-7" alt="">
+       </div>
+         <!-- Formulaire -->
+       <div class="row">
+       <div class="col-3"></div>
+       <div class="col">
+              <div class="card border border-primary">
+                      <div class="card-header bg-primary">
+                        Ajout Apprenant
+                      </div>
+                      <div class="card-body">
+                        
+                      </div>
+              </div>
+       
+       </div>
+       <div class="col-3"></div>
+       
+       </div>
+     </div> 
+
+<style text="text/css">
+.bg-primary{
+  background-color:#068689!important;
+}
+.img-fluid {
+    max-width: 7%!important;
+    height: auto;
+}
+.border-primary{
+  border-color: black!important;
+  border-width: 2px !important;
+}
+</style>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
