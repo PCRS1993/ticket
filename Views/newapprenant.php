c8a678ec03ec8ebcd7c5e10a0005ac95b5bf7b9d
makuenzy <oumardrame09@gmail.com>
2019-10-15 11:44:22 +0000

    corps ecran

--- Views/newapprenant.php
+++ Views/newapprenant.php
@@ -1,157 +1,88 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <title>nouveau apprenant</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-</head>
-<body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<div class="container-fluid">
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-<ul class="navbar-nav">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Utilisateurs</a>
-    </li>
-    <li class="nav-item dropdown active">
-    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Sessions</a>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Liste session</a>
-      <a class="dropdown-item" href="#">Nouvelle session</a>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+     <div class="container-fluid px-0">
+     <!-- Barre de Navigation -->
       
-    </div>
-  </li>
-  <li class="nav-item dropdown active">
-    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Référentiel</a>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Liste referentiel</a>
-      <a class="dropdown-item" href="#">Nouveau</a>
-      
-    </div>
-
-    
-
-  </li>
-  </ul>
-
-
+       <nav class="navbar navbar-expand-lg navbar-success bg-primary">
+       
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown link
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+        </div>
 </nav>
-</div>
-<img src="../Public/img/logo.jpg"  style="width: 10%;" alt="logo.jpg"> 
-
-<div class="container" style= "border: solid black 1px;height: 1050px;">
-
-<div class="row">
-<div class="col-12 text-center">
-<h2>Ajout Apprenant</h2>
-</div>
-<div class="col-12 col-md-4 offset-md-4">
-    <hr class="text-success">
-</div>
-    <div class="col-12 col-md-6 offset-md-3">
-    <form action="/action_page.php" class="was-validated">
-  <div class="form-group">
-    <label for="uname">Nom:</label>
-    <input type="text" class="form-control" id="uname" placeholder="Entrer nom" name="uname" required>
-    <div class="valid-feedback">Valid.</div>
-
-
-  <div class="form-group">
-    <label for="uname">Prénom:</label>
-    <input type="text" class="form-control" id="uname" placeholder="Entrer prénom" name="uname" required>
-    <div class="valid-feedback">Valid.</div>
-
-<div class="form-group">
-    <label for="uname">Email:</label>
-    <input type="text" class="form-control" id="uname" placeholder="Entrer email" name="uname" required>
-    <div class="valid-feedback">Valid.</div>
-
-    <div class="form-group">
-    <label for="uname">Adresse:</label>
-    <input type="text" class="form-control" id="uname" placeholder="Enntrer adresse" name="uname" required>
-    <div class="valid-feedback">Valid.</div>
-
-    <div class="form-group">
-    <label for="uname">Téléphone:</label>
-    <input type="text" class="form-control" id="uname" placeholder="Entrer téléphone" name="uname" required>
-    <div class="valid-feedback">Valid.</div>  
-
- <div class="form-group">
-    <label for="uname">Téléphone tuteur:</label>
-    <input type="text" class="form-control" id="uname" placeholder="Entrer Tél tuteur" name="uname" required>
-    <div class="valid-feedback">Valid.</div>
-<div class="form-group">
-<label for="uname">Statut:</label>
- <select name="cars" class="custom-select">
-    <option selected></option>
-    <option value="volvo">Présent</option>
-    <option value="fiat">Abandonné</option>
-
-  </select> </div>
-
-  <div class="form-group">
-<label for="uname">Session:</label>
- <select name="cars" class="custom-select">
-    <option selected></option>
-    <option value="volvo">2020</option>
-    <option value="fiat">2021</option>
-    <option value="volvo">2022</option>
-    <option value="fiat">2023</option>
-
-  </select> </div>
-
-  <div class="form-group">
-<label for="uname">Référentiel:</label>
- <select name="cars" class="custom-select">
-    <option selected></option>
-    <option value="volvo">Référent digital</option>
-    <option value="fiat">Data artisan</option>
-    <option value="volvo">Dev Web</option>
-
-
-  </select> </div> 
-
-
-  <div class="form-group">
-    <label for="uname">Login:</label>
-    <input type="text" class="form-control" id="uname" placeholder="Enter username" name="uname" required>
-    <div class="valid-feedback">Valid.</div>
-  </div>
-  
-  <div class="form-group">
-    <label for="pwd">Password:</label>
-    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd" required>
-    <div class="valid-feedback">Valid.</div>
-
-  </div>
-  <div class="form-group form-check">
-    <label class="form-check-label">
       
-      <div class="valid-feedback">Valid.</div>
-      <div class="invalid-feedback">Check this checkbox to continue.</div>
-    </label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+         <!-- Logo-->
+       <div class="row">
+           <img src="../Public/img/logo.jpg" class="img-fluid mw-7" alt="">
+       </div>
+         <!-- Formulaire -->
+       <div class="row">
+       <div class="col-2"></div>
+       <div class="col">
+              <div class="card border border-primary">
+                      <div class="card-header bg-primary">
+                        Ajout Apprenant
+                      </div>
+                      <div class="card-body">
+                        
+                      </div>
+              </div>
+       
+       </div>
+       <div class="col-3"></div>
+       
+       </div>
+     </div> 
 
-    </div>
-</div>
-
-
-
-  </div>
-</div>
-
-
-
-
-
-
-
-
-</body>
+<style text="text/css">
+.bg-primary{
+  background-color:#068689!important;
+}
+.img-fluid {
+    max-width: 7%!important;
+    height: auto;
+}
+.border-primary{
+  border-color: black!important;
+  border-width: 2px !important;
+}
+</style>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
