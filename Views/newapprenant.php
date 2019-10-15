96f6148fb0685eab00778b515a4b4de8d96660be
makuenzy <oumardrame09@gmail.com>
2019-10-15 16:09:01 +0000

    corps ecran2

--- Views/newapprenant.php
+++ Views/newapprenant.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Title</title>
+    <title>New Apprenant</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -12,51 +12,153 @@
   <body>
      <div class="container-fluid px-0">
      <!-- Barre de Navigation -->
-      
-       <nav class="navbar navbar-expand-lg navbar-success bg-primary">
-       
+        
+     <nav class="navbar navbar-expand-lg navbar-success bg-primary">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown link
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-</nav>
-      
+         <span class="navbar-toggler-icon"></span>
+       </button>
+
+       <div class="collapse navbar-collapse col-3" id="navbarNavDropdown">
+         <ul class="navbar-nav">
+           
+             <a class="nav-link dropdown-toggle text-primary" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Utilisateurs
+             </a>
+             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+               <a class="dropdown-item" href="#">Nouveau</a>
+               <a class="dropdown-item" href="#">Liste</a>
+
+         </div>
+             </ul>
+
+             <ul class="navbar-nav">
+           
+           <a class="nav-link dropdown-toggle text-primary" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+             Sessions
+           </a>
+           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+             <a class="dropdown-item" href="#">Promo 3</a>
+             <a class="dropdown-item" href="#">Promo 4</a>
+             <a class="dropdown-item" href="#">Promo 5</a>
+             <a class="dropdown-item" href="#">Promo 6</a>
+             <a class="dropdown-item" href="#">Promo 7</a>
+             <a class="dropdown-item" href="#">Promo 8</a>
+
+       </div>
+           </ul>      
+           <ul class="navbar-nav">
+           
+           <a class="nav-link dropdown-toggle text-primary" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+             Référents
+           </a>
+           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+             <a class="dropdown-item" href="#">Référent digital</a>
+             <a class="dropdown-item" href="#">Dev Web</a>
+             <a class="dropdown-item" href="#">Data artisan</a>
+
+       </div>
+           </ul>      
+                                          
+       </div>
+</nav>      
+           
          <!-- Logo-->
        <div class="row">
            <img src="../Public/img/logo.jpg" class="img-fluid mw-7" alt="">
        </div>
          <!-- Formulaire -->
        <div class="row">
-       <div class="col-3"></div>
+       <div class="col-5 justify-content-center"></div>
        <div class="col">
               <div class="card border border-primary">
                       <div class="card-header bg-primary">
+                      <div class= "text-center text-primary font-weight-bold">
                         Ajout Apprenant
+                        </div>
                       </div>
                       <div class="card-body">
-                        
+                      <form action="/action_page.php" class="was-validated">
+  <div class="form-group">
+    <label for="uname">Nom:</label>
+    <input type="text" class="form-control" id="uname" placeholder="Entrer nom" name="uname" required>
+    <div class="valid-feedback">Valid.</div> <br>
+
+
+  <div class="form-group">
+    <label for="uname">Prénom:</label>
+    <input type="text" class="form-control" id="uname" placeholder="Entrer prénom" name="uname" required>
+    <div class="valid-feedback">Valid.</div> <br>
+
+<div class="form-group">
+    <label for="uname">Email:</label>
+    <input type="text" class="form-control" id="uname" placeholder="Entrer email" name="uname" required>
+    <div class="valid-feedback">Valid.</div>
+
+    <div class="form-group">
+    <label for="uname">Adresse:</label>
+    <input type="text" class="form-control" id="uname" placeholder="Entrer adresse" name="uname" required>
+    <div class="valid-feedback">Valid.</div>
+
+    <div class="form-group">
+    <label for="uname">Téléphone:</label>
+    <input type="text" class="form-control" id="uname" placeholder="Entrer téléphone" name="uname" required>
+    <div class="valid-feedback">Valid.</div>  
+
+ <div class="form-group">
+    <label for="uname">Téléphone tuteur:</label>
+    <input type="text" class="form-control" id="uname" placeholder="Entrer Tél tuteur" name="uname" required>
+    <div class="valid-feedback">Valid.</div>
+
+<div class="form-group">
+<label for="uname">Statut:</label>
+ <select name="statut" class="custom-select">
+    <option selected></option>
+    <option value="">Présent</option>
+    <option value="">Abandonné</option>
+</select> </div>
+
+<div class="form-group">
+<label for="uname">Session:</label>
+ <select name="session" class="custom-select">
+    <option selected></option>
+    <option value="">2020</option>
+    <option value="">2021</option>
+    <option value="">2022</option>
+    <option value="">2023</option>
+</select> </div>
+
+  <div class="form-group">
+<label for="uname">Référentiel:</label>
+ <select name="referentiel" class="custom-select">
+    <option selected></option>
+    <option value="">Référent digital</option>
+    <option value="">Data artisan</option>
+    <option value="">Dev Web</option>
+</select> </div> 
+
+
+<div class="form-group">
+    <label for="uname">Login:</label>
+    <input type="text" class="form-control" id="uname" placeholder="Enter username" name="uname" required>
+    <div class="valid-feedback">Valid.</div>
+  </div>
+  
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd" required>
+    <div class="valid-feedback">Valid.</div>
+
+  </div>
+ <div class="btn1 inline"> 
+  <button type="submit">Annuler</button>
+
+  </div> 
+  <div class="btn2 inline"> 
+  <button type="submit">Valider</button>
+  </div> 
+
+</form>
+  
                       </div>
               </div>
        
@@ -77,7 +179,14 @@
 .border-primary{
   border-color: black!important;
   border-width: 2px !important;
+
 }
+
+.text-primary{
+  color: white!important;
+}
+
+n
 </style>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
